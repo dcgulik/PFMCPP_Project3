df70080d2dc0b27733b5c17ee91b9e9b4ccdc0f3
dcgulik <38480967+dcgulik@users.noreply.github.com>
2023-05-20 23:42:38 +0000

    Part 1e - Step 8: declared member functions and variables

--- main.cpp
+++ main.cpp
@@ -438,14 +438,22 @@ struct ExerciseBike
 {
 // 5 properties:
 //     1) number of seat adjustments (int)
+int numSeatAdjustments = 12;
 //     2) maximum resistance (float)
+float maximumResistance = 15.0f;
 //     3) number of program modes (int)
+int numProgramModes = 8;
 //     4) number of pulse readers (int)
+int numPulseReaders = 2;
 //     5) power consumed (float)
+int powerConsumed = 12.0f;
 // 3 things it can do:
 //     1) begin a workout program
+void beginWorkoutProgram(int selectedProgram);
 //     2) read pulse
+float readPulse(); // returns pulse in beats per minute
 //     3) calculate caloric burn
+float calculateCaloricBurn(float weight); // returns total calories burned during session
 };
 
 // Thing 2) department store
@@ -455,14 +463,23 @@ struct DepartmentStore
 {
 // 5 properties:
 //     1) number of employees (int)
+int numEmployees = 70;
 //     2) number of departments (int)
+int numDepartments = 16;
 //     3) amount of electricity used per month (float)
+float electricityPerWeek = 1200.0f;
 //     4) amount of profit per week (int)
+int profitPerWeek = 60000;
 //     5) name (std::string)
+std::string name = "David's";
 // 3 things it can do:
 //     1) open/close 
+bool open(); // returns true to denote open store
+bool close(); // returns false to denote closed store
 //     2) host a sale
+void hostSale(float discountRate);
 //     3) sell an item to a customer
+float sellItem(); // returns the total sale price
 };
 
 // Thing 3) golf course
@@ -472,14 +489,23 @@ struct GolfCourse
 {
 // 5 properties:
 //     1) number of holes (int)
+int numHoles = 18;
 //     2) number of golfers per week (double)
-//     3) size, in square feet (float)
+double golfersPerWeek = 15000;
+//     3) length, in yards (float)
+float courseLength = 6500;
 //     4) number of golfers per party (int)
+int golfersPerParty = 4;
 //     5) members-only (or not) (bool)
+bool membersOnly = false;
 // 3 things it can do:
 //     1) host a tournament
+void hostTournament(int numParticipants, int tournamentDate, float purse, float firstPlacePercentage, float secondPlacePercentage, float thirdPlacePercentage);
 //     2) suspend play for weather reasons
+bool suspendPlay(float duration); // returns false to suspend play for the specific duration; would then return back to true, resuming play 
 //     3) change hole location on green
+void changeHoleLocation(int holeNum, float latitude, float longitude); 
+
 };
 
 // Thing 4) library
@@ -489,14 +515,22 @@ struct Library
 {
 // 5 properties:
 //     1) number of books (double)
+double numBooks = 35000;
 //     2) number of library cards/users (double)
+double totalLibraryCards = 10000;
 //     3) number of employees/volunteers (int)
-//     4) book/media checkout periods (int)
-//     5) late fees, amounts, etc. (float)
+int numEmployees = 20;
+//     4) book/media checkout periods, in days (int)
+int checkoutPeriod = 14;
+//     5) late fees, amounts, etc. per day (float)
+float lateFee = .35f; 
 // 3 things it can do:
 //     1) host a book sale
+void hostBookSale(int date, int numBooks, std::string saleName);
 //     2) check a book out to a library card
+bool checkOutBook(); // returns true to denote book as checked out
 //     3) suspend privileges to user with delinquent checkouts/fees
+bool suspendCheckoutPrivileges(); // returns false to suspend user privileges until determined threshold is met
 };
     
 // Thing 5) Drumhead
@@ -505,15 +539,24 @@ struct Library
 struct Drumhead
 {
 // 5 properties:
-//     1) diameter (int)
-//     2) ply thickness (float)
+//     1) diameter, in inches (int)
+int diameter = 14;
+//     2) ply thickness, in mm (float)
+float plyThickness = 10.0f;
 //     3) number of plies (int)
+int plies = 1;
 //     4) brand (std::string)
+std::string brand = "Remo";
 //     5) material (std::string)
+std::string material = "film";
 // 3 things it can do:
-//     1) add muffling
+//     1) wrinkle
+void wrinkle();
 //     2) break
+bool breakDrumhead(); // returns true to alert SnareDrum struct that something is broken
 //     3) tighten/loosen
+void tighten(float tension);
+void loosen(float tension);
 };
 
 // Thing 6) Snare Wires
@@ -523,14 +566,22 @@ struct SnareWires
 {
 // 5 properties:
 //     1) number of wires (int)
+int numWires = 20;
 //     2) material (std::string)
-//     3) wire gauge (float)
+std::string material = "steel";
+//     3) wire gauge, in mm (float)
+float wireGauge = .6f;
 //     4) brand (std::string)
+std::string brand = "Puresound";
 //     5) with straps/without straps (bool)
+bool withStraps = false;
 // 3 things it can do:
 //     1) crack
+void crack();
 //     2) buzz
+void buzz();
 //     3) break a wire
+bool breakSnareWires(); // returns true to alert SnareDrum struct that something is broken
 };
 
 // Thing 7) Snare Strainer
@@ -539,15 +590,23 @@ struct SnareWires
 struct SnareStrainer
 {
 // 5 properties:
-//     1) mounting distance (float)
+//     1) mounting distance, in inches (float)
+float mountingDistance = 2.0f;
 //     2) number of mounting screws (int)
-//     3) maximum tension (float)
+int numMountingScrews = 2;
+//     3) maximum tension, in poundsforce (float)
+float maximumTension = 12.5f;
 //     4) brand (std::string)
+std::string brand = "Ludwig";
 //     5) type (std::string)
+std::string strainerType = "side-action";
 // 3 things it can do:
 //     1) engage snares
+bool engageSnares(); // returns true to snareWires
 //     2) disengage snares
+bool disengageSnares(); // returns false to snareWires
 //     3) tighten snares
+float tightenSnares(SnareWires* snareWires, float tension); // tightens or loosens snareWires by given tension
 };
 
 // Thing 8) Drum Shell
@@ -557,14 +616,22 @@ struct DrumShell
 {
 // 5 properties:
 //     1) brand (std::string)
+std::string brand = "Pearl";
 //     2) material (std::string)
-//     3) diameter (int)
+std::string material = "birch";
+//     3) diameter, in inches (int)
+int diameter = 14;
 //     4) bearing edge angle (float)
-//     5) depth (int)
+float bearingEdgeAngle = 45.0f;
+//     5) depth, in inches (float)
+float depth = 5.5f;
 // 3 things it can do:
 //     1) crack
+bool crackShell(); // returns true to alert SnareDrum struct that something is broken
 //     2) be vented
+void beVented(int numHoles, float holeDiameter);
 //     3) resonate
+void resonate(float velocity);
 };
 
 // Thing 9) Lugs
@@ -574,14 +641,22 @@ struct Lug
 {
 // 5 properties:
 //     1) brand (std::string)
-//     2) length (int)
+std::string brand = "Pearl";
+//     2) length (float)
+float length = 2.0f;
 //     3) single-sided? (bool)
-//     4) SKU (std::string)
+bool singleSided = false;
+//     4) decoupled? (bool)
+bool decoupled = true;
 //     5) material (std::string)
+std::string material = "steel";
 // 3 things it can do:
 //     1) tighten drum head
+void tightenDrumhead(float tension);
 //     2) loosen drum head
+void loosenDrumhead(float tension);
 //     3) overtighten
+bool overtighten(); // returns true to alert SnareDrum struct that something is broken
 };
 
 // Thing 10) Snare Drum
@@ -590,15 +665,23 @@ struct Lug
 struct SnareDrum
 {
 // 5 properties:
-//     1) Head
-//     2) Wires
-//     3) Strainer
-//     4) Shell
+//     1) Drumhead
+Drumhead drumhead;
+//     2) Snare Wires
+SnareWires snareWires;
+//     3) Snare Strainer
+SnareStrainer snareStrainer;
+//     4) Drumshell
+DrumShell drumshell;
 //     5) Lugs
+Lug lug1, lug2, lug3, lug4, lug5, lug6, lug7, lug8;
 // 3 things it can do:
 //     1) drive backbeat
+void driveBackbeat(float bpm);
 //     2) define drum sound
+void defineDrumSound();
 //     3) break
+bool breakDrum(); // returns true until fixed
 };
 
 
