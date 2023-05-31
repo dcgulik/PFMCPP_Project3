a935b70e875be643c3f5bb06bda21a88d3d595e0
dcgulik <38480967+dcgulik@users.noreply.github.com>
2023-05-31 18:21:52 +0000

    Added implementations for member functions

--- main.cpp
+++ main.cpp
@@ -108,9 +108,50 @@ struct CarWash
     You'll need to insert the Person struct from the video in the space below.
  */
 
+struct Person 
+{
+    int age;
+    int height;
+    float hairLength;
+    float GPA;
+    unsigned int SATScore;
+    int distanceTraveled;
 
+    void run(int howFast, bool startWithLeftFoot);
 
+    struct Foot
+    {
+        int stepSize() 
+        {
+            return 7;
+        }
+    
+        void stepForward(int howFast)
+        {
+            std::cout<<"Stepped forward"<<stepSize()*howFast<<"."<<std::endl;
+        }
+    };
 
+    Foot leftFoot, rightFoot;
+
+};
+
+void Person::run(int howFast, bool startWithLeftFoot)
+{
+    if (startWithLeftFoot)
+    {
+        leftFoot.stepForward(howFast);
+        rightFoot.stepForward(howFast);
+    }
+    else
+    {
+        rightFoot.stepForward(howFast);
+        leftFoot.stepForward(howFast);
+    }
+    
+    distanceTraveled += leftFoot.stepSize() + rightFoot.stepSize();
+
+}
 
  /*
  2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
@@ -131,286 +172,411 @@ struct CarWash
  This usually means you have to use identical types for all variables used in an expression that is producing that conversion warning.
  */
 
-
-// Thing 1) exercise bike 
 struct ExerciseBike
 {
-// 5 properties:
-//     1) number of seat adjustments (int)
     int numSeatAdjustments = 12;
-//     2) maximum resistance (float)
     float maximumResistance = 15.0f;
-//     3) number of program modes (int)
     int numProgramModes = 8;
-//     4) number of pulse readers (int)
     int numPulseReaders = 2;
-//     5) power consumed (float)
     int powerConsumed = 12.0f;
-// 3 things it can do:
-//     1) begin a workout program
+
     void beginWorkoutProgram(int selectedProgram);
-//     2) read pulse
-    float readPulse(); // returns pulse in beats per minute
-//     3) calculate caloric burn
-    float calculateCaloricBurn(float weight); // returns total calories burned during session
+    float readPulse();
+    float calculateCaloricBurn(float weight, float timeSpent);
 };
 
-// Thing 2) department store
+void ExerciseBike::beginWorkoutProgram(int selectedProgram)
+{
+    std::cout<<"Beginning Workout #"<<selectedProgram<<std::endl;
+}
+
+float ExerciseBike::readPulse()
+{
+    float pulseRead = 30.0f;
+    return pulseRead * 3.0f;
+}
+
+float ExerciseBike::calculateCaloricBurn(float weight, float timeSpent)
+{
+    std::cout<<"Calculating caloric burn..."<<std::endl;
+    return weight * readPulse() * timeSpent;
+}
+
 struct DepartmentStore
 {
-// 5 properties:
-//     1) number of employees (int)
     int numEmployees = 70;
-//     2) number of departments (int)
     int numDepartments = 16;
-//     3) amount of electricity used per month (float)
     float electricityPerWeek = 1200.0f;
-//     4) amount of profit per week (int)
     int profitPerWeek = 60000;
-//     5) name (std::string)
     std::string name = "David's";
-// 3 things it can do:
-//     1) open/close 
-    bool open(); // returns true to denote open store
 
-    bool close(); // returns false to denote closed store
-//     2) host a sale
+    bool open();
+    bool close();
     void hostSale(float discountRate);
-//     3) sell an item to a customer
-    float sellItem(); // returns the total sale price
+    float sellItem(std::string itemName, float price, float taxRate, bool isOnSale, float discountRate);
 };
 
-// Thing 3) golf course
+bool DepartmentStore::open()
+{
+    return 1;
+}
+
+bool DepartmentStore::close()
+{
+    return 0;
+}
+
+void DepartmentStore::hostSale(float discountRate)
+{
+    std::cout<<"We're having a sale! Enjoy "<<discountRate<<"% off!"<<std::endl;
+}
+
+float DepartmentStore::sellItem(std::string itemName, float price, float taxRate, bool isOnSale, float discountRate)
+{
+    float totalPrice = price;
+    if(isOnSale) totalPrice -= (totalPrice * discountRate);
+    totalPrice += (totalPrice * taxRate);
+    
+    std::cout<<"Sold!"<<itemName<<" has sold for "<<totalPrice<<std::endl;
+    
+    return totalPrice;
+}
 
 struct GolfCourse
 {
+    int numHoles = 18;
+    double golfersPerWeek = 15000;
+    float courseLength = 6500;
+    int golfersPerParty = 4;
+    bool membersOnly = false;
+
     struct Golfer
     {
-//     5 properties:
-//     1) Name (std::string)
         std::string name = " ";
-//     2) Handicap (int)
         int handicap = 0;
-//     3) Years of Experience (int)
         int yearsOfExperience = 0;
-//     4) Tournament winner? (bool)
         bool tournamentWinner = false;
-//     5) Tournaments won (int)
         int tournamentsWon = 0;
-//     3 things it can do:
-//     1) drive
-        float drive(float power, float accuracy); // returns distance to tee
-//     2) putt
-        float putt(float power, float accuracy); // returns distance to tee
-//     3) chip
-        float chip(float power, float accuracy); // returns distance to tee
+
+        float drive(float power, float accuracy, float distanceToTee);
+        float putt(float power, float accuracy, float distanceToTee);
+        float chip(float power, float accuracy, float distanceToTee);
     };
-// 5 properties:
-//     1) number of holes (int)
-    int numHoles = 18;
-//     2) number of golfers per week (double)
-    double golfersPerWeek = 15000;
-//     3) length, in yards (float)
-    float courseLength = 6500;
-//     4) number of golfers per party (int)
-    int golfersPerParty = 4;
-//     5) members-only (or not) (bool)
-    bool membersOnly = false;
-// 3 things it can do:
-//     1) host a tournament
+
     void hostTournament(int numParticipants, int tournamentDate, float purse, float firstPlacePercentage,         float secondPlacePercentage, float thirdPlacePercentage); 
-//     2) suspend play for weather reasons
-    bool suspendPlay(float duration); // returns false to suspend play for the specific duration; would then return back to true, resuming play 
-//     3) change hole location on green
+    bool suspendPlay(float duration);
     void changeHoleLocation(int holeNum, float latitude, float longitude, Golfer tester); 
 
     Golfer golfPro;
 };
 
-// Thing 4) library
+float GolfCourse::Golfer::drive(float power, float accuracy, float distanceToTee)
+{
+    return distanceToTee - (power * distanceToTee * .75f * accuracy);
+}
+
+float GolfCourse::Golfer::putt(float power, float accuracy, float distanceToTee)
+{
+    return distanceToTee - (power * distanceToTee * .25f * accuracy);
+}
+
+float GolfCourse::Golfer::chip(float power, float accuracy, float distanceToTee)
+{
+    return distanceToTee - (power * distanceToTee * .50f * accuracy);
+}
+
+void GolfCourse::hostTournament(int numParticipants, int tournamentDate, float purse, float firstPlacePercentage, float secondPlacePercentage, float thirdPlacePercentage)
+{
+    std::cout<<"It's a golf tournament!"<<std::endl;
+    std::cout<<numParticipants<<" will gather on "<<tournamentDate<<"to compete for "<<purse<<" in winnings!"<<std::endl;
+    std::cout<<"1st prize will win: "<<purse * firstPlacePercentage<<std::endl;
+    std::cout<<"2nd prize will win: "<<purse * secondPlacePercentage<<std::endl;
+    std::cout<<"3rd prize will win: "<<purse * thirdPlacePercentage<<std::endl;
+}
+
+bool GolfCourse::suspendPlay(float duration)
+{
+    if(duration == 0.0f) 
+    {
+        std::cout<<"No suspension necessary!";
+        return 1;
+    }
+    else
+    {
+        std::cout<<"Play will be suspended for "<<duration<<std::endl;
+        return 0;
+    }
+}
+
+void GolfCourse::changeHoleLocation(int holeNum, float latitude, float longitude, Golfer tester)
+{
+    std::cout<<"Hole at #"<<holeNum<<" has been moved to "<<latitude<<"x"<<longitude<<". "<<tester.name<<" has verified fair placement."<<std::endl;
+}
+
 struct Library
 {
+    double numBooks = 35000;
+    double totalLibraryCards = 10000;
+    int numEmployees = 20;
+    int checkoutPeriod = 14;
+    float lateFee = .35f; 
+
     struct LibraryCard
     {
-//    5 properties:
-//     1) ID number (int)
         int iDNumber = 000000000;
-//     2) Name (std::string)
         std::string name = " ";
-//     3) Number of books checked out (int)
         int booksCheckedOut = 0;
-//     4) Fees due (float)
         float feesDue = 0.0f;
-//     5) Registration date (double)
         double registrationDate = 00000000;
-// 3 things it can do:
-//     1) return a list of books checked out
-        std::string allBooksCheckedOut(); // returns a printout of titles of all books             checked out by that card
-//     2) calculate total fees owed over the past year
-        float calculateYearlyFeesOwed(int year); // returns total fees owed for given              year
-//     3) grant access to computer terminal
+
+        std::string allBooksCheckedOut();
+        float calculateYearlyFeesOwed(int year);
         void grantComputerAccess(float duration = 1.0f);
     };
-// 5 properties:
-//     1) number of books (double)
-    double numBooks = 35000;
-//     2) number of library cards/users (double)
-    double totalLibraryCards = 10000;
-//     3) number of employees/volunteers (int)
-    int numEmployees = 20;
-//     4) book/media checkout periods, in days (int)
-    int checkoutPeriod = 14;
-//     5) late fees, amounts, etc. per day (float)
-    float lateFee = .35f; 
-// 3 things it can do:
-//     1) host a book sale
-    void hostBookSale(double date, int numBooks, std::string saleName);
-//     2) check a book out to a library card
-    bool checkOutBook(LibraryCard libraryCard); // returns true to denote book as checked out
-//     3) suspend privileges to user with delinquent checkouts/fees
-    bool suspendCheckoutPrivileges(LibraryCard libraryCard); // returns false to suspend user privileges until determined threshold is met
+
+    void hostBookSale(double date, int numBooksForSale, std::string saleName);
+    bool checkOutBook(LibraryCard libraryCard);
+    bool suspendCheckoutPrivileges(LibraryCard libraryCard); 
 
     LibraryCard memberOfTheMonth;  
 };
 
-// Thing 5) Drumhead
+std::string Library::LibraryCard::allBooksCheckedOut()
+{
+    std::string result = "These are all the books checked out by ";
+    result += std::to_string(iDNumber);
+    return result;
+}
+
+float Library::LibraryCard::calculateYearlyFeesOwed(int year)
+{
+    std::cout<<iDNumber<<"owed "<<feesDue<<" in "<<year<<"."<<std::endl;
+    return feesDue;
+}
+
+void Library::LibraryCard::grantComputerAccess(float duration)
+{
+    std::cout<<"Success! Computer access granted for "<<duration<<"hours. Enjoy!"<<std::endl;
+}
+
+void Library::hostBookSale(double date, int numBooksForSale, std::string saleName)
+{
+    std::cout<<"It's a book sale! "<<saleName<<" will be held on "<<date<<". Choose from "<<numBooksForSale<<"books and support your local library!"<<std::endl;
+}
+
+bool Library::checkOutBook(LibraryCard libraryCard)
+{
+    if(libraryCard.feesDue > 1000000.0f)
+    {
+        std::cout<<"Error: "<<libraryCard.iDNumber<<"owes more than one million dollars in late fees!";
+        return 0;
+    }
+    else
+    {
+        std::cout<<"Book checked out to"<<libraryCard.iDNumber;
+        return 1;
+    }
+}
+
+bool Library::suspendCheckoutPrivileges(LibraryCard libraryCard)
+{
+    if (libraryCard.feesDue > 1000000.0f)
+    {
+        std::cout<<"Fees due total over one million dollars. Checkout privileges suspended."<<std::endl;
+        return 0;
+    }
+    else
+    {
+        std::cout<<"Fees insufficient for suspension. :("<<std::endl;
+        return 1;
+    }
+}
+
+
 struct Drumhead
 {
-// 5 properties:
-//     1) diameter, in inches (int)
     int diameter = 14;
-//     2) ply thickness, in mm (float)
     float plyThickness = 10.0f;
-//     3) number of plies (int)
     int plies = 1;
-//     4) brand (std::string)
     std::string brand = "Remo";
-//     5) material (std::string)
     std::string material = "film";
-// 3 things it can do:
-//     1) wrinkle
+
     void wrinkle();
-//     2) break
-    bool breakDrumhead(); // returns true to alert SnareDrum struct that something is broken
-//     3) tighten/loosen
+    bool breakDrumhead();
     void tighten(float tension);
     void loosen(float tension);
 };
 
-// Thing 6) Snare Wires
+void Drumhead::wrinkle()
+{
+    std::cout<<"Drumhead has wrinkled..."<<std::endl;
+}
+
+bool Drumhead::breakDrumhead()
+{
+    std::cout<<"Drumhead has broken!"<<std::endl;
+    return 1;
+}
+
+void Drumhead::tighten(float tension)
+{
+    std::cout<<"Drumhead has been tightened by "<<tension<<"."<<std::endl;
+}
+
+void Drumhead::loosen(float tension)
+{
+    std::cout<<"Drumhead has been loosened by "<<tension<<"."<<std::endl;
+}
+
 struct SnareWires
 {
-// 5 properties:
-//     1) number of wires (int)
     int numWires = 20;
-//     2) material (std::string)
     std::string material = "steel";
-//     3) wire gauge, in mm (float)
     float wireGauge = .6f;
-//     4) brand (std::string)
     std::string brand = "Puresound";
-//     5) with straps/without straps (bool)
     bool withStraps = false;
-// 3 things it can do:
-//     1) crack
+
     void crack();
-//     2) buzz
     void buzz();
-//     3) break a wire
-    bool breakSnareWires(); // returns true to alert SnareDrum struct that something is broken
+    bool breakSnareWires();
 };
 
-// Thing 7) Snare Strainer
+void SnareWires::crack()
+{
+    std::cout<<"Crack!"<<std::endl;
+}
+
+void SnareWires::buzz()
+{
+    std::cout<<"Bzzzzzzz..."<<std::endl;
+}
+
+bool SnareWires::breakSnareWires()
+{
+    std::cout<<"Snare wires have broken!"<<std::endl;
+    return 1;
+}
+
 struct SnareStrainer
 {
-// 5 properties:
-//     1) mounting distance, in inches (float)
     float mountingDistance = 2.0f;
-//     2) number of mounting screws (int)
     int numMountingScrews = 2;
-//     3) maximum tension, in poundsforce (float)
     float maximumTension = 12.5f;
-//     4) brand (std::string)
     std::string brand = "Ludwig";
-//     5) type (std::string)
     std::string strainerType = "side-action";
-// 3 things it can do:
-//     1) engage snares
-    bool engageSnares(); // returns true to snareWires
-//     2) disengage snares
-    bool disengageSnares(); // returns false to snareWires
-//     3) tighten snares
-    float tightenSnares(SnareWires* snareWires, float tension); // tightens or loosens snareWires by given tension
+
+    bool engageSnares();
+    bool disengageSnares();
+    float tightenSnares(SnareWires* snareWires, float tension);
 };
 
-// Thing 8) Drum Shell
+bool SnareStrainer::engageSnares()
+{
+    std::cout<<"Snares engaged!"<<std::endl;
+    return 1;
+}
+
+bool SnareStrainer::disengageSnares()
+{
+    std::cout<<"Snares disengaged."<<std::endl;
+    return 0;
+}
+
+float SnareStrainer::tightenSnares(SnareWires *snareWires, float tension)
+{
+    std::cout<<snareWires<<"have been tightened by "<<tension<<"."<<std::endl;
+    return tension;
+}
+
+
 struct DrumShell
 {
-// 5 properties:
-//     1) brand (std::string)
     std::string brand = "Pearl";
-//     2) material (std::string)
     std::string material = "birch";
-//     3) diameter, in inches (int)
     int diameter = 14;
-//     4) bearing edge angle (float)
     float bearingEdgeAngle = 45.0f;
-//     5) depth, in inches (float)
     float depth = 5.5f;
-// 3 things it can do:
-//     1) crack
-    bool crackShell(); // returns true to alert SnareDrum struct that something is broken
-//     2) be vented
+
+    bool crackShell();
     void beVented(int numHoles, float holeDiameter);
-//     3) resonate
     void resonate(float velocity);
 };
 
-// Thing 9) Lugs
+bool DrumShell::crackShell()
+{
+    std::cout<<"Drum shell has cracked!"<<std::endl;
+    return 1;
+}
+
+void DrumShell::beVented(int numHoles, float holeDiameter)
+{
+    std::cout<<"Drum shell has been vented with "<<numHoles<<"holes of "<<holeDiameter<<" in. in diameter."<<std::endl;
+}
+
+void DrumShell::resonate(float velocity)
+{
+    if(velocity < 35.0f) std::cout<<"no resonance..."<<std::endl;
+    else std::cout<<"resonance with "<<velocity<<" velocity!"<<std::endl;
+}
+
 struct Lug
 {
-// 5 properties:
-//     1) brand (std::string)
     std::string brand = "Pearl";
-//     2) length (float)
     float length = 2.0f;
-//     3) single-sided? (bool)
     bool singleSided = false;
-//     4) decoupled? (bool)
     bool decoupled = true;
-//     5) material (std::string)
     std::string material = "steel";
-// 3 things it can do:
-//     1) tighten drum head
+
     void tightenDrumhead(float tension);
-//     2) loosen drum head
     void loosenDrumhead(float tension);
-//     3) overtighten
-    bool overtighten(); // returns true to alert SnareDrum struct that something is broken
+    bool overtighten();
 };
 
-// Thing 10) Snare Drum
+void Lug::tightenDrumhead(float tension)
+{
+    std::cout<<"Drumhead tightened by "<<tension<<"."<<std::endl;
+}
+
+void Lug::loosenDrumhead(float tension)
+{
+    std::cout<<"Drumhead loosened by "<<tension<<"."<<std::endl;
+}
+
+bool Lug::overtighten()
+{
+    std::cout<<"Lug has overtightened!"<<std::endl;
+    return 1;
+}
+
 struct SnareDrum
 {
-// 5 properties:
-//     1) Drumhead
     Drumhead drumhead;
-//     2) Snare Wires
     SnareWires snareWires;
-//     3) Snare Strainer
     SnareStrainer snareStrainer;
-//     4) Drumshell
     DrumShell drumshell;
-//     5) Lugs
     Lug lug1, lug2, lug3, lug4, lug5, lug6, lug7, lug8;
-// 3 things it can do:
-//     1) drive backbeat
+
     void driveBackbeat(float bpm);
-//     2) define drum sound
     void defineDrumSound();
-//     3) break
-    bool breakDrum(); // returns true until fixed
+    bool breakDrum();
 };
+
+void SnareDrum::driveBackbeat(float bpm)
+{
+    std::cout<<"Ready? 1, 2, 3, 4..."<<std::endl;
+    std::cout<<"That's a groovy "<<bpm<<" bpm backbeat!"<<std::endl;
+}
+
+void SnareDrum::defineDrumSound()
+{
+    std::cout<<"Wow, what a unique drum sound!"<<std::endl;
+}
+
+bool SnareDrum::breakDrum()
+{
+    std::cout<<"Snare drum broken!"<<std::endl;
+    return 1;
+}
 
 int main()
 {
